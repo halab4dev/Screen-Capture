7d4f47abb4c05bff35de18fb5b24d1d88609875a
HaLAB <halab4it@gmail.com>
2016-07-24 10:13:56 +0700

    Push project to github

---
+++ src/main/PictureFormat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Apollo
+ */
+public class PictureFormat {
+
+    public static final String JPG = "JPG";
+    public static final String BMP = "BMP";
+    public static final String PNG = "PNG";
+}
---
+++ src/main/ScreenCapturer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Apollo
+ */
+public class ScreenCapturer {
+
+    private static final Rectangle SCREEN_RECTANGLE = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+
+    public static void createScreenCapture(File output, String format) throws RuntimeException {
+        try {
+            BufferedImage capture = new Robot().createScreenCapture(SCREEN_RECTANGLE);
+            output.mkdirs();
+            ImageIO.write(capture, format, output);
+        } catch (IOException | AWTException ex) {
+            Logger.getLogger(ScreenCapturer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
